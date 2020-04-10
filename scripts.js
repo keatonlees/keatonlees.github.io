ca535693429478b82b58262b6c5acce4c7bd5d59
Keaton Lees <keatslees@yahoo.com>
2020-04-10 15:51:07 -0700

    Placeholder images

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 var x = document.getElementById("navTop");
 
 function nav_toggle() {
-	if (x.className === "nav-top") {
-		x.className += " responsive";
-	} else {
-		x.className = "nav-top";
-	}
+    if (x.className === "nav-top") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-top";
+    }
 }
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 100) {
+        console.log("OK");
+    }
+});
