80f7248a61dc2471ca822b318999e32b41c9de5c
Keaton Lees <keatslees@yahoo.com>
2020-05-08 16:29:49 -0700

    Overhaul

--- scripts.js
+++ scripts.js
@@ -1,21 +1,3 @@
-// script for responsive website
-var x = document.getElementById("navTop");
-
-function nav_toggle() {
-    if (x.className === "nav-top") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav-top";
-    }
-}
-
-// script for having navbar appear as user scrolls down
-// $(window).scroll(function() {
-//     if ($(window).scrollTop() > 50) {
-
-// 	}
-// });
-
 // script for smooth scrolling
 $(document).ready(function() {
     $("a").on('click', function(event) {
@@ -30,3 +12,43 @@ $(document).ready(function() {
         }
     });
 });
+
+// script for typing animation
+const name_span = document.querySelector('.name-typing-text');
+const description_span = document.querySelector('.description-typing-text');
+const name_cursor = document.querySelector('.name-cursor');
+const description_cursor = document.querySelector('.description-cursor');
+const character_delay = 100;
+let name_char_index = 0;
+let description_char_index = 0;
+
+const name = "Hi! I'm Keaton";
+const description = "A Systems Design Engineering Student";
+
+function type() {
+    if (name_char_index < name.length) {
+        name_span.textContent += name.charAt(name_char_index);
+        name_char_index++;
+        setTimeout(type, character_delay);
+    } else if (description_char_index < description.length) {
+        description_span.textContent += description.charAt(description_char_index);
+        description_char_index++;
+        setTimeout(type, character_delay);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(type, 1000)
+});
+
+// script for about-slideshow 
+$("#about-slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+    $('#about-slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#about-slideshow')
+}, 4000);
