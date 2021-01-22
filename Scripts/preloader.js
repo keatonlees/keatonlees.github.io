803d460102a2c4409482f4b4b612db7876c8c8e6
Keaton Lees <keatslees@yahoo.com>
2021-01-21 23:50:53 -0500

    Website Revamp Maintenance

---
+++ Scripts/preloader.js
@@ -0,0 +1,13 @@
+// var preloader_time = 1500;
+// $(window).on("load", function () {
+//   setTimeout(function () {
+//     $(".preloader-container").fadeOut();
+//     onload_function();
+//   }, preloader_time);
+// });
+
+// function onload_function() {
+//   setTimeout(function () {
+//     $(".main-content").fadeIn();
+//   }, 500);
+// }
